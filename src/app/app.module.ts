d8423a1f3cac56776e82a138d455ebc3b343d3dd
saiffardin <saiffardin@gmail.com>
2022-04-19 18:28:24 +0600

    material tabs added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatTreeModule } from '@angular/material/tree';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FsUiComponent } from './fs-ui/fs-ui.component';
+import { TabComponent } from './fs-ui/tab/tab.component';
 
 @NgModule({
-  declarations: [AppComponent, FsUiComponent],
+  declarations: [AppComponent, FsUiComponent, TabComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -19,6 +25,12 @@ import { FsUiComponent } from './fs-ui/fs-ui.component';
     HttpClientModule,
     MatMenuModule,
     MatSnackBarModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/fs-ui/tab/tab.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css'],
+})
+export class TabComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  tabs = ['First', 'Second', 'Third'];
+  selected = new FormControl(0);
+
+  addTab(selectAfterAdding: boolean) {
+    this.tabs.push('New');
+
+    if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+}
